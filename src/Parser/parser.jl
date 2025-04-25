0db3ce845199566842f5681407c51114ca989343
Hannes <hannes10042004@icloud.com>
2025-04-25 17:33:58 +0200

    Finished parser implementation

--- src/Parser/parser.jl
+++ src/Parser/parser.jl
@@ -5,19 +5,46 @@ struct Rule
     rhs::Vector{Vector{String}}
 end
 
-function parse()
+function parse(lexer::Lexer, grammer::Vector{String})
+    if peek(lexer).token_kind == EOLToken
+        return
+    end
+    lhs = expect(lexer, NonTerminalToken)
+    if lhs.content in grammer
+        throw(ParseError("Symbol $(lhs.content) already defined", lhs.location))
+    else
+        push!(grammer, lhs.content)
+    end
+    expect(lexer, MetaToken)
     
+    rhs = parse_rhs(lexer)
+    return Rule(lhs.content, rhs)
 end
 
-function parse_rhs()
-
-end
+function parse_rhs(lexer::Lexer)
+    rhs = []
+    options = String[]
+    
+    while peek(lexer).token_kind != EOLToken
+        tok = next(lexer)
+        
+        if tok.token_kind == PundentToken
+            push!(rhs, options)
+            options = []
+        else
+            push!(options, tok.content)
+        end
+    end
+    
+    push!(rhs, options)
+    return rhs
+ end
 
 function expect(lexer::Lexer, expected::TokenKind)
     v = next(lexer)
-    println(v)
+
     if v.token_kind != expected
         throw(ParseError("Expected $(expected), but got $(v)", v.location))
     end
-    return true
+    return v
 end
--- src/bnf.jl
+++ src/bnf.jl
@@ -7,7 +7,9 @@ for x in ARGS
     if isfile(x)
         lines = readlines(x)
         for i in eachindex(lines)
-        
+            grammer = String[]
+            lexer = init_lexer(lines[i], x, i)
+            println(parse(lexer, grammer))
         end
       
     end
